84b77c9410c5377afe271d249cc3f34b808fdd61
marco0225 <classwitch@naver.com>
2024-10-15 13:16:58 +0900

    feat: Pinia를 사용하여 기능 구현

--- src/components/common/pagination/Pagination.vue
+++ src/components/common/pagination/Pagination.vue
@@ -1,19 +1,19 @@
 <template>
-    <Pagination v-slot="{ page }" :total="total >= 1000 ? 1000 : total" :itemsPerPage="30" :sibling-count="1" show-edges :default-page="1">
+    <Pagination :total="total >= 1000 ? 1000 : total" :itemsPerPage="30" :sibling-count="1" show-edges :default-page="currentPage">
         <PaginationList v-slot="{ items }" class="flex items-center gap-1">
             <PaginationFirst @click="changePage(1)" />
-            <PaginationPrev @click="changePage(currenPage - 1)" />
+            <PaginationPrev @click="changePage(currentPage - 1)" />
 
-            <template v-for="(item, index) in items">
-                <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
-                    <Button class="w-10 h-10 p-0" :variant="item.value === page ? 'default' : 'outline'" @click="changePage(item.value - 1)">
+            <template v-for="(item, index) in items" :key="index">
+                <PaginationListItem v-if="item.type === 'page'" :value="item.value" as-child>
+                    <Button class="w-10 h-10 p-0" :variant="item.value === currentPage ? 'default' : 'outline'" @click="changePage(item.value)">
                         {{ item.value }}
                     </Button>
                 </PaginationListItem>
-                <PaginationEllipsis v-else :key="item.type" :index="index" />
+                <PaginationEllipsis v-else :index="index" />
             </template>
 
-            <PaginationNext @click="changePage(currenPage + 1)" />
+            <PaginationNext @click="changePage(currentPage + 1)" />
             <PaginationLast @click="changePage(totalPages)" />
         </PaginationList>
     </Pagination>
@@ -42,7 +42,7 @@ export default defineComponent({
         return { store: useStore() };
     },
     computed: {
-        currenPage() {
+        currentPage() {
             return this.store.page;
         },
     },
